eea7f90c103955bf580384472041f2a943e8e1fd
6thSence <d.pushkarskaya.pr@gmail.com>
2016-04-16 20:24:36 +0600

    scripts

---
+++ src/works/scripts.js
@@ -0,0 +1,32 @@
+const scrollTop = function () {
+
+    const _scrollTo = (element, to, duration) => {
+        if (duration <= 0) return;
+        const difference = to - element.scrollTop;
+        const perTick = difference / duration * 10;
+
+        setTimeout(() => {
+            element.scrollTop = element.scrollTop + perTick;
+            if (element.scrollTop === to) return;
+            _scrollTo(element, to, duration - 10);
+        }, 10);
+    }
+
+    const _scrollTop = () => {
+        _scrollTo(document.body, 0, 400);
+    };
+
+    const _eventListener = () => {
+        document.getElementById('button-scroll-top').addEventListener('click', _scrollTop);
+    };
+
+    const init = () => {
+        _eventListener();
+    }
+
+    return {
+        init
+    }
+}();
+
+scrollTop.init();
